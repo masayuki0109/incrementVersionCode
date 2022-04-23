ec90dd2e1f276a5191948cdff8a7e3054d3f09cf
Masayuki Yamaji <afact_of_1ife@yahoo.co.jp>
2022-04-23 20:20:00 +0900

    ファイル名を変更できるように

--- incrementMajorVersion.sh
+++ incrementMajorVersion.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-cat app.config.js | awk '/versionCode:/ {print "sed -i \"\" \"s/versionCode: "$2"/versionCode: "(int($2 / 10000) + 1) * 10000",/\" app.config.js"}' | awk '{system($0)}'
-cat app.config.js | awk '/version:/ {gsub(/["|,]/, "", $2);print $2}' | awk -F "." '{print "sed -i \"\" " "s/"$1"."$2"."$3"/"$1+1".0.0/g app.config.js"}' | awk '{system($0)}'
+CONFIG=`./setDefaultFile.sh $1`
+cat $CONFIG | awk -v config=$CONFIG '/versionCode:/ {print "sed -i \"\" \"s/versionCode: "$2"/versionCode: "(int($2 / 10000) + 1) * 10000",/\" "config}' | awk '{system($0)}'
+cat $CONFIG | awk '/version:/ {gsub(/["|,]/, "", $2);print $2}' | awk -v config=$CONFIG  -F "." '{print "sed -i \"\" " "s/"$1"."$2"."$3"/"$1+1".0.0/g "config}' | awk '{system($0)}'
--- incrementPatchVersion.sh
+++ incrementPatchVersion.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-cat app.config.js | awk '/versionCode:/ {print "sed -i \"\" \"s/versionCode: "$2"/versionCode: "$2 + 1",/\" app.config.js"}' | awk '{system($0)}'
-cat app.config.js | awk '/version:/ {gsub(/["|,]/, "", $2);print $2}' | awk -F "." '{print "sed -i \"\" " "s/"$1"."$2"."$3"/"$1"."$2"."$3+1"/g app.config.js"}' | awk '{system($0)}'
+CONFIG=`./setDefaultFile.sh $1`
+cat $CONFIG | awk -v config=$CONFIG '/versionCode:/ {print "sed -i \"\" \"s/versionCode: "$2"/versionCode: "$2 + 1",/\" "config}' | awk '{system($0)}'
+cat $CONFIG | awk '/version:/ {gsub(/["|,]/, "", $2);print $2}' | awk -v config=$CONFIG  -F "." '{print "sed -i \"\" " "s/"$1"."$2"."$3"/"$1"."$2"."$3+1"/g "config}' | awk '{system($0)}'
